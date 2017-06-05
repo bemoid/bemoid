e0ccc54f21594b6e2d9f65e971e418e1b1f4662f
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2017-06-05 18:11:02 +0200

    Reorganize folder structure; Adds utilities modules; Fixes docblock comments

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,7 @@
 const autoprefixer = require('autoprefixer')
-const linter = require('postcss-bem-linter')
 
 module.exports = {
   plugins: [
-    linter({
-      componentSelectors: (block, options) => {
-        const namespace = (options && options.namespace) ? options.namespace + '-' : '';
-        const word = '[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*';
-        const element = '(?:__' + word + ')?';
-        const modifier = '(?:--' + word + '){0,2}';
-        const attribute = '(?:\\[.+\\])?';
-
-        return new RegExp('^\\.' + namespace + block + element + modifier + attribute + '$');
-      }
-    }),
     autoprefixer
   ]
 }
